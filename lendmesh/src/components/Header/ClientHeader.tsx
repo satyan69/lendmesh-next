57c578b0b0274257da4dfd36a0ddc4310856967d
Seshu Vuggina <seshuggina@gmail.com>
2025-05-06 22:10:17 +0530

    Add LoginPopup component and integrate into ClientHeader; enhance user authentication flow

--- lendmesh/src/components/Header/ClientHeader.tsx
+++ lendmesh/src/components/Header/ClientHeader.tsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import "./Header.scss";
+import LoginPopup from "../Login/Login";
 
 export default function ClientHeader({ menuData }: { menuData: any[] }) {
   const [isOpen, setIsOpen] = useState(false);
   const [openSubmenus, setOpenSubmenus] = useState<Record<string, boolean>>({});
   const [isScrolled, setIsScrolled] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
   useEffect(() => {
     document.body.style.overflow = isOpen ? "hidden" : "auto";
@@ -25,134 +27,43 @@ export default function ClientHeader({ menuData }: { menuData: any[] }) {
   };
 
   return (
-    <nav
-      className={`w-full sm:px-3 px-6 transition-colors duration-300 ease-in-out ${
-        isScrolled ? "bg-[#002144] shadow-md" : "bg-transparent"
-      }`}
-    >
-      <div className="lg:container md:mx-auto md:mx-0 w-full md:max-w-full flex justify-between items-center">
-        {/* Logo */}
-        <Link
-          href="/"
-          className="flex items-center space-x-2 py-4 lm-nav-brand"
-        >
-          <Image
-            className="dark:invert"
-            src={"/images/logo.svg"}
-            width={150}
-            height={30}
-            alt="Lendmesh"
-            priority
-          />
-        </Link>
-
-        {/* Desktop nav */}
-        <ul className="hidden md:flex space-x-1 lg:space-x-8 text-white nav-items-wrap">
-          {menuData.map((menuItem) => (
-            <li
-              key={menuItem.id}
-              className={menuItem.submenu ? "relative group" : ""}
-            >
-              <Link
-                href={menuItem.path ?? "#"}
-                className="py-6 px-2 hover:text-green-400 transition inline-flex w-full justify-center gap-x-1.5 items-center text-base md:text-sm lg:text-base"
-              >
-                {menuItem.title}
-                {menuItem.submenu && (
-                  <svg
-                    className="-mr-1 size-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M5.22 8.22a.75.75 0 011.06 0L10 11.94l3.72-3.72a.75.75 0 011.06 1.06l-4.25 4.25a.75.75 0 01-1.06 0L5.22 9.28a.75.75 0 010-1.06Z"
-                    />
-                  </svg>
-                )}
-              </Link>
-              {menuItem.submenu && (
-                <div className="absolute hidden group-hover:block bg-white text-black rounded-lg shadow-md w-48 top-full left-0 space-y-2 py-2">
-                  {menuItem.submenu.map((subItem: any) => (
-                    <Link
-                      key={subItem.id}
-                      href={subItem.path ?? "#"}
-                      className="block px-4 py-2 hover:bg-[#002144] hover:text-white"
-                    >
-                      {subItem.title}
-                    </Link>
-                  ))}
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
-
-        {/* Sign up btn */}
-        <div className="hidden md:block">
-          <button className="bg-[#6BE3A2] text-white px-4 lg:px-6 py-2 text-sm rounded-[8px] shadow-lg hover:bg-[#6BE3A2] transition whitespace-nowrap cursor-pointer">
-            Sign Up
-          </button>
-        </div>
-
-        {/* Hamburger Button */}
-        <button
-          className="md:hidden text-white focus:outline-none"
-          onClick={toggleMenu}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-8 w-8"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
-      </div>
-
-      {/* Mobile Menu */}
-      <div
-        className={`md:hidden ${
-          isOpen ? "block" : "hidden"
-        } bg-white text-[#002144] fixed inset-0 z-50`}
+    <>
+      <nav
+        className={`w-full sm:px-3 px-6 transition-colors duration-300 ease-in-out ${
+          isScrolled ? "bg-[#002144] shadow-md" : "bg-transparent"
+        }`}
       >
-        <div className="flex justify-end p-4">
-          <button
-            type="button"
-            onClick={() => setIsOpen(false)}
-            className="text-5xl text-[#002144] hover:text-[#002144] transition cursor-pointer"
+        <div className="lg:container md:mx-auto md:mx-0 w-full md:max-w-full flex justify-between items-center">
+          {/* Logo */}
+          <Link
+            href="/"
+            className="flex items-center space-x-2 py-4 lm-nav-brand"
           >
-            &times;
-          </button>
-        </div>
-        <ul className="flex flex-col space-y-4 p-6">
-          {menuData.map((menuItem) => (
-            <li key={menuItem.id}>
-              {!menuItem.submenu ? (
+            <Image
+              className="dark:invert"
+              src={"/images/logo.svg"}
+              width={150}
+              height={30}
+              alt="Lendmesh"
+              priority
+            />
+          </Link>
+
+          {/* Desktop nav */}
+          <ul className="hidden md:flex space-x-1 lg:space-x-8 text-white nav-items-wrap">
+            {menuData.map((menuItem) => (
+              <li
+                key={menuItem.id}
+                className={menuItem.submenu ? "relative group" : ""}
+              >
                 <Link
                   href={menuItem.path ?? "#"}
-                  className="hover:text-blue-400 transition font-semibold px-2 py-1 block"
-                  onClick={() => setIsOpen(false)}
+                  className="py-6 px-2 hover:text-green-400 transition inline-flex w-full justify-center gap-x-1.5 items-center text-base md:text-sm lg:text-base"
                 >
                   {menuItem.title}
-                </Link>
-              ) : (
-                <>
-                  <button
-                    onClick={() => toggleSubmenu(menuItem.id)}
-                    className="hover:text-blue-400 transition font-semibold w-full px-2 py-1 flex justify-between items-center"
-                  >
-                    {menuItem.title}
+                  {menuItem.submenu && (
                     <svg
-                      className="w-5 h-5 text-gray-400"
+                      className="-mr-1 size-5"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                     >
@@ -162,45 +73,145 @@ export default function ClientHeader({ menuData }: { menuData: any[] }) {
                         d="M5.22 8.22a.75.75 0 011.06 0L10 11.94l3.72-3.72a.75.75 0 011.06 1.06l-4.25 4.25a.75.75 0 01-1.06 0L5.22 9.28a.75.75 0 010-1.06Z"
                       />
                     </svg>
-                  </button>
-                  <ul
-                    className={`mt-2 space-y-2 p-4 bg-gray-100 text-[#002144] rounded-lg ${
-                      openSubmenus[menuItem.id] ? "block" : "hidden"
-                    }`}
-                  >
+                  )}
+                </Link>
+                {menuItem.submenu && (
+                  <div className="absolute hidden group-hover:block bg-white text-black rounded-lg shadow-md w-48 top-full left-0 space-y-2 py-2">
                     {menuItem.submenu.map((subItem: any) => (
-                      <li key={subItem.id}>
-                        <Link
-                          href={subItem.path ?? "#"}
-                          className="block hover:text-blue-400 transition"
-                          onClick={() => setIsOpen(false)}
-                        >
-                          {subItem.title}
-                        </Link>
-                      </li>
+                      <Link
+                        key={subItem.id}
+                        href={subItem.path ?? "#"}
+                        className="block px-4 py-2 hover:bg-[#002144] hover:text-white"
+                      >
+                        {subItem.title}
+                      </Link>
                     ))}
-                  </ul>
-                </>
-              )}
-            </li>
-          ))}
-        </ul>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
 
-        <div className="p-6 text-center">
-          <a
-            href="#"
-            className="w-full block bg-[#6BE3A2] text-[#002144] px-6 py-2 rounded-lg shadow-lg hover:text-[#002144]"
+          {/* Sign up btn */}
+          <div className="hidden md:block">
+            <button
+              onClick={() => setShowLogin(true)}
+              className="bg-[#6BE3A2] text-white px-4 lg:px-6 py-2 text-sm rounded-[8px] shadow-lg hover:bg-[#6BE3A2] transition whitespace-nowrap cursor-pointer"
+            >
+              Sign Up
+            </button>
+          </div>
+
+          {/* Hamburger Button */}
+          <button
+            className="md:hidden text-white focus:outline-none"
+            onClick={toggleMenu}
           >
-            Sign Up
-          </a>
-          <small>
-            Already a lendmesh member?{" "}
-            <a href="#" className="underline">
-              Login
-            </a>
-          </small>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
         </div>
-      </div>
-    </nav>
+
+        {/* Mobile Menu */}
+        <div
+          className={`md:hidden ${
+            isOpen ? "block" : "hidden"
+          } bg-white text-[#002144] fixed inset-0 z-50`}
+        >
+          <div className="flex justify-end p-4">
+            <button
+              type="button"
+              onClick={() => setIsOpen(false)}
+              className="text-5xl text-[#002144] hover:text-[#002144] transition cursor-pointer"
+            >
+              &times;
+            </button>
+          </div>
+          <ul className="flex flex-col space-y-4 p-6">
+            {menuData.map((menuItem) => (
+              <li key={menuItem.id}>
+                {!menuItem.submenu ? (
+                  <Link
+                    href={menuItem.path ?? "#"}
+                    className="hover:text-blue-400 transition font-semibold px-2 py-1 block"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    {menuItem.title}
+                  </Link>
+                ) : (
+                  <>
+                    <button
+                      onClick={() => toggleSubmenu(menuItem.id)}
+                      className="hover:text-blue-400 transition font-semibold w-full px-2 py-1 flex justify-between items-center"
+                    >
+                      {menuItem.title}
+                      <svg
+                        className="w-5 h-5 text-gray-400"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M5.22 8.22a.75.75 0 011.06 0L10 11.94l3.72-3.72a.75.75 0 011.06 1.06l-4.25 4.25a.75.75 0 01-1.06 0L5.22 9.28a.75.75 0 010-1.06Z"
+                        />
+                      </svg>
+                    </button>
+                    <ul
+                      className={`mt-2 space-y-2 p-4 bg-gray-100 text-[#002144] rounded-lg ${
+                        openSubmenus[menuItem.id] ? "block" : "hidden"
+                      }`}
+                    >
+                      {menuItem.submenu.map((subItem: any) => (
+                        <li key={subItem.id}>
+                          <Link
+                            href={subItem.path ?? "#"}
+                            className="block hover:text-blue-400 transition"
+                            onClick={() => setIsOpen(false)}
+                          >
+                            {subItem.title}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+
+          <div className="p-6 text-center">
+            <a
+              onClick={() => setShowLogin(true)}
+              href="#"
+              className="w-full block bg-[#6BE3A2] text-[#002144] px-6 py-2 rounded-lg shadow-lg hover:text-[#002144]"
+            >
+              Sign Up
+            </a>
+            <small>
+              Already a lendmesh member?{" "}
+              <a href="#" className="underline">
+                Login
+              </a>
+            </small>
+          </div>
+        </div>
+      </nav>
+      {/* Login Popup */}
+      {showLogin && <LoginPopup hideLogin={setShowLogin} />}
+      {/* {showLogin && <LoginPopup />} */}
+    </>
   );
 }
---
+++ lendmesh/src/components/Login/Login.tsx
@@ -0,0 +1,248 @@
+import React from "react";
+
+interface LoginPopupProps {
+  hideLogin: (value: boolean) => void;
+}
+
+const LoginPopup: React.FC<LoginPopupProps> = ({ hideLogin }) => {
+  const close = () => {
+    hideLogin(false);
+  };
+
+  return (
+    <>
+      <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-50 z-50">
+        <div className="flex max-w-4xl w-[98%] bg-white rounded-xl shadow-lg relative">
+          {/* Close button */}
+          <button
+            className="absolute top-[-11px] right-[-11px] w-10 text-3xl h-10 cursor-pointer rounded-full bg-white shadow-md flex items-center justify-center text-gray-500 hover:text-[#002144] hover:shadow-lg transition duration-300 ease-in-out"
+            aria-label="Close"
+            onClick={close}
+          >
+            &times;
+          </button>
+
+          {/* Left image panel */}
+          <div
+            className="w-1/2 bg-cover bg-center hidden md:flex items-center justify-center text-white text-center"
+            style={{ backgroundImage: `url('/your-image.jpg')` }}
+          >
+            <div className="bg-black bg-opacity-50 p-6 rounded-tl-[8px] rounded-bl-[8px] h-full w-full flex flex-col items-center justify-center">
+              <h2 className="text-2xl font-semibold">
+                Hello
+                <br />
+                Loan Seeker!
+              </h2>
+              <p className="mt-2 text-sm">
+                Your easy loan journey starts right here
+              </p>
+            </div>
+          </div>
+
+          {/* Right form panel */}
+          <div className="w-full md:w-1/2 p-8">
+            <h2 className="text-2xl font-bold text-gray-800 mb-2 text-center">
+              Welcome Back
+            </h2>
+            <p className="text-sm  mb-6 text-center">
+              Don't have an account?{" "}
+              <a
+                href="#"
+                className="font-semibold text-[#4296EA] hover:underline"
+              >
+                Sign up for free
+              </a>
+            </p>
+
+            {/* Email input */}
+            <div className="mb-4">
+              <label htmlFor="email" className="sr-only">
+                Email Id
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Email Id*"
+                className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+
+            {/* Password input */}
+            <div className="mb-2">
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                className="w-full border border-gray-300 rounded-[8px] px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+
+            {/* Forgot Password */}
+            <div className="text-center mb-4">
+              <a href="#" className="text-sm hover:underline">
+                Forgot Password?
+              </a>
+            </div>
+
+            {/* Sign in button */}
+            <button className="w-full bg-[#6BE3A2] text-white px-4 lg:px-6 py-2 text-sm rounded-[8px] shadow-lg hover:bg-[#6BE3A2] transition whitespace-nowrap cursor-pointer">
+              Sign In
+            </button>
+
+            {/* Divider */}
+            <div className="flex items-center my-4">
+              <div className="flex-grow h-px bg-gray-300"></div>
+              <span className="px-2 text-gray-400 text-sm">or</span>
+              <div className="flex-grow h-px bg-gray-300"></div>
+            </div>
+
+            {/* Google Sign In */}
+            <button className="w-full border border-gray-300 flex items-center justify-center py-2 rounded hover:bg-gray-50 cursor-pointer">
+              <img
+                src="https://www.svgrepo.com/show/475656/google-color.svg"
+                alt="Google"
+                className="w-5 h-5 mr-2"
+              />
+              Sign up with Google
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* {
+// 
+// Sign Up Popup - remove Hidden class to show this popup
+// 
+// } */}
+      <div className="hidden fixed inset-0 flex items-center justify-center bg-white bg-opacity-50 z-50">
+        <div className="flex max-w-4xl w-[98%] bg-white rounded-xl shadow-lg relative">
+          {/* Close button */}
+          <button
+            className="absolute top-[-11px] right-[-11px] w-10 text-3xl h-10 cursor-pointer rounded-full bg-white shadow-md flex items-center justify-center text-gray-500 hover:text-[#002144] hover:shadow-lg transition duration-300 ease-in-out"
+            aria-label="Close"
+            onClick={close}
+          >
+            &times;
+          </button>
+
+          {/* Left image panel */}
+          <div
+            className="w-1/2 bg-cover bg-center hidden md:flex items-center justify-center text-white text-center"
+            style={{ backgroundImage: `url('/your-image.jpg')` }}
+          >
+            <div className="bg-black bg-opacity-50 p-6 rounded-tl-[8px] rounded-bl-[8px] h-full w-full flex flex-col items-center justify-center">
+              <h2 className="text-2xl font-semibold">
+                Hello
+                <br />
+                Loan Seeker!
+              </h2>
+              <p className="mt-2 text-sm">
+                Your easy loan journey starts right here
+              </p>
+            </div>
+          </div>
+
+          {/* Right form panel */}
+          <div className="w-full md:w-1/2 p-8">
+            <h2 className="text-2xl font-bold text-gray-800 mb-2 text-center">
+              Create an Account
+            </h2>
+            <p className="text-sm  mb-6 text-center">
+              Already have an account?{" "}
+              <a
+                href="#"
+                className="font-semibold text-[#4296EA] hover:underline"
+              >
+                login
+              </a>
+            </p>
+
+            {/* Email input */}
+            <div className="mb-4">
+              <label htmlFor="firstName" className="sr-only">
+                First Name
+              </label>
+              <input
+                type="text"
+                id="firstName"
+                placeholder="First Name *"
+                className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="lastName" className="sr-only">
+                Last Name
+              </label>
+              <input
+                type="text"
+                id="lastName"
+                placeholder="Last Name *"
+                className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="email" className="sr-only">
+                Last Name
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Email *"
+                className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+
+            {/* Password input */}
+            <div className="mb-2">
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                className="w-full border border-gray-300 rounded-[8px] px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+
+            {/* Forgot Password */}
+            <div className="text-center mb-4">
+              <a href="#" className="text-sm hover:underline">
+                Forgot Password?
+              </a>
+            </div>
+
+            {/* Sign in button */}
+            <button className="w-full bg-[#6BE3A2] text-white px-4 lg:px-6 py-2 text-sm rounded-[8px] shadow-lg hover:bg-[#6BE3A2] transition whitespace-nowrap cursor-pointer">
+              Sign In
+            </button>
+
+            {/* Divider */}
+            <div className="flex items-center my-4">
+              <div className="flex-grow h-px bg-gray-300"></div>
+              <span className="px-2 text-gray-400 text-sm">or</span>
+              <div className="flex-grow h-px bg-gray-300"></div>
+            </div>
+
+            {/* Google Sign In */}
+            <button className="w-full border border-gray-300 flex items-center justify-center py-2 rounded hover:bg-gray-50">
+              <img
+                src="https://www.svgrepo.com/show/475656/google-color.svg"
+                alt="Google"
+                className="w-5 h-5 mr-2"
+              />
+              Sign up with Google
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPopup;
